27a11cfaf13e1df4bf29ac49bda1f026fd5bb90a
Beth Schofield <bethmschofield@gmail.com>
2017-11-14 00:49:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(names) {
-  names.map(function(name) {
+  return names.map(function(name) {
     let fName = name.split(" ")[0];
     let lName = name.split(" ")[1];
     return {firstName: fName, lastName: lName};
   });
 }
-    
+
 
 
 
