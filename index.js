0d3fa6844f753a54404c6d1cb375390a81804faa
Beth Schofield <bethmschofield@gmail.com>
2017-11-14 00:24:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    return drivers.map(function(exname) { drivers["name"].toUpperCase
+  };
+})
+
+
+
+// [1, 2, 3].map(function (num) { return num * num; });
+// // => [1, 4, 9]
