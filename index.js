8deedddd4a6c0c0ff1640c4bf3e5024e9ca8a39c
Beth Schofield <bethmschofield@gmail.com>
2017-11-14 00:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-    lowerCaseDriverArray = drivers.map(function(exname) { exname.toLowerCase
+    let lowerCaseDriverArray = drivers.map(function(exdriver) { 
+      exdriver["name"].toLowerCase;
   });
   return lowerCaseDriverArray;
 }
@@ -9,3 +10,11 @@ function lowerCaseDrivers(drivers) {
 
 // [1, 2, 3].map(function (num) { return num * num; });
 // // => [1, 4, 9]
+
+function matchName(drivers, string) {
+  let matchedNames = drivers.filter(function(driver){
+      // return drivers["name"] === drivername;
+      return driver["name"].toUpperCase() === string.toUpperCase();
+    });
+return matchedNames;
+}
