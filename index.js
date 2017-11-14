2aff5d6f0fa48dd716ede06a51336dafa0c1eb94
Beth Schofield <bethmschofield@gmail.com>
2017-11-14 00:27:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-    let lowerCaseDriverArray = drivers.map(function(exdriver) { 
+    let lowerCaseDriverArray = drivers.map(function(exdriver) {
       exdriver["name"].toLowerCase;
   });
   return lowerCaseDriverArray;
@@ -11,10 +11,10 @@ function lowerCaseDrivers(drivers) {
 // [1, 2, 3].map(function (num) { return num * num; });
 // // => [1, 4, 9]
 
-function matchName(drivers, string) {
-  let matchedNames = drivers.filter(function(driver){
-      // return drivers["name"] === drivername;
-      return driver["name"].toUpperCase() === string.toUpperCase();
-    });
-return matchedNames;
-}
+// function matchName(drivers, string) {
+//   let matchedNames = drivers.filter(function(driver){
+//       // return drivers["name"] === drivername;
+//       return driver["name"].toUpperCase() === string.toUpperCase();
+//     });
+// return matchedNames;
+// }
